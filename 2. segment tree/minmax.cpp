7e5e0f7497d0f5bd13e4004c7226fb0b8f2a63f7
HyunCha <truetype24@gmail.com>
2020-07-11 18:03:32 +0900

    졸려

--- 2. segment tree/minmax.cpp
+++ 2. segment tree/minmax.cpp
@@ -2,44 +2,49 @@
 using namespace std;
 typedef long long int ll;
 struct Tree {
-    vector<ll> node;
+    vector<ll> node_max;
+    vector<ll> node_min;
     ll update_min (ll node_num, ll s, ll e, ll x, ll value) {
-        if (x < s || x > e) return node[node_num];
-        if (s == e) return node[node_num] = value;
+        if (x < s || x > e) return node_min[node_num];
+        if (s == e) return node_min[node_num] = value;
         ll mid = (s + e) / 2;
-        return node[node_num] = min(update (2 * node_num, s, mid, x, value), update (2 * node_num + 1, mid + 1, e, x, value));
+        return node_min[node_num] = min(update_min (2 * node_num, s, mid, x, value), update_min  (2 * node_num + 1, mid + 1, e, x, value));
     }
     ll update_max (ll node_num, ll s, ll e, ll x, ll value) {
-        if (x < s || x > e) return node[node_num];
-        if (s == e) return node[node_num] = value;
+        if (x < s || x > e) return node_max[node_num];
+        if (s == e) return node_max[node_num] = value;
         ll mid = (s + e) / 2;
-        return node[node_num] = max(update (2 * node_num, s, mid, x, value), update (2 * node_num + 1, mid + 1, e, x, value));
+        return node_max[node_num] = max(update_max (2 * node_num, s, mid, x, value), update_max (2 * node_num + 1, mid + 1, e, x, value));
     }
     ll query_min (ll node_num, ll s, ll e, ll l, ll r) {
         if (e < l || r < s) return 0;
-        if (l <= s && e <= r) return node[node_num];
+        if (l <= s && e <= r) return node_min[node_num];
         ll mid = (s + e) / 2;
-        return min (query (2 * node_num, s, mid, l, r), query (2 * node_num + 1, mid + 1, e, l, r));
+        return min (query_min (2 * node_num, s, mid, l, r), query_min (2 * node_num + 1, mid + 1, e, l, r));
     }
     ll query_max (ll node_num, ll s, ll e, ll l, ll r) {
         if (e < l || r < s) return 0;
-        if (l <= s && e <= r) return node[node_num];
+        if (l <= s && e <= r) return node_max[node_num];
         ll mid = (s + e) / 2;
-        return max (query (2 * node_num, s, mid, l, r), query (2 * node_num + 1, mid + 1, e, l, r));
+        return max (query_max (2 * node_num, s, mid, l, r), query_max (2 * node_num + 1, mid + 1, e, l, r));
     }
 }node;
 
 int main () {
     int n, m;
     cin >> n >> m;
-    node.node.resize(4*n);
+    node.node_min.resize(4*n);
+    node.node_max.resize(4*n);
     for (int i = 1; i <= n; ++i) {
         int a;
         cin >> a;
-        node.update(1, 1, n, i, a);
+        node.update_min(1, 1, n, i, a);
+        node.update_max(1, 1, n, i, a);
     }
     for (int i = 0; i < m; ++i) {
         int a, b;
-
+        cin >> a >> b;
+        cout << node.query_min(1, 1, n, a, b) << " " << node.query_max(1, 1, n, a, b) << "\n";
     }
+    return 0;
 }
